cec82a52108e46a292b67788ab12c03e920611b5
1998-droid <tiotiotio98@gmail.com>
2022-12-23 10:14:23 +0700

    c

--- manage/data.php
+++ manage/data.php
@@ -39,8 +39,10 @@
                                     
                                         <tr>
                                         <td><?php echo $data['n_dok']; ?></td>
-                                            <td><?php echo $data['subag']; ?></td>
-                                            <td><?php echo $data['file']; ?></td>
+                                        <td><?php echo $data['tahun']; ?></td>
+                                        <td><?php echo $data['nomor']; ?></td>
+                                        <td><?php echo $data['subag']; ?></td>
+                                        <td><?php echo $data['file']; ?></td>
                                             <td><a href="preview.php?id=<?php echo $data['id_dok'];?>" class="btn btn-primary btn-sm" ><i class="fa fa-info fa-fw" aria-hidden="true"></i></a>
                                             <a href="hps.php?id=<?php echo $data['id_dok'];?>" onclick="return confirm('Yakin Hapus?')" class="btn btn-danger btn-sm" ><i class="fa fa-trash fa-fw" aria-hidden="true"></i></a>
                                             <a href="unduh.php?file=<?php echo $data['file'];?>" class="btn btn-warning btn-sm" ><i class="fa fa-cloud-download fa-fw" aria-hidden="true"></i></a>
