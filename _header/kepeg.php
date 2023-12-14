c8a83010f47a709c9272d835eaa12f09d45035e7
1998-droid <tiotiotio98@gmail.com>
2023-12-14 08:56:55 +0700

    headerkepeg

--- _header/kepeg.php
+++ _header/kepeg.php
@@ -40,7 +40,7 @@ if ($_SESSION['role'] == "") {
 </head>
 
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-blue">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
         <!-- Navbar Brand-->
         <a class="navbar-brand ps-3" href="../index.html"><b>Repository</b></a>
         <!-- Sidebar Toggle-->
